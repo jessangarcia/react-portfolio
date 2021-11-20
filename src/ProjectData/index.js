8158b9edec4b8167e936bf54171914937908dcb2
jessan <jessenia.garcia998@gmail.com>
2021-11-20 13:34:31 -0700

    added project data array

---
+++ src/ProjectData/index.js
@@ -0,0 +1,48 @@
+import GG from '../assets/images/gamego.png';
+import dnd from '../assets/images/dnd.png';
+import salonme from '../assets/images/salonme.png';
+import weather from '../assets/images/weathershot.png';
+import runbud from '../assets/images/runbuddy.png';
+import budget from '../assets/images/budget-tracker.png';
+
+const data = [
+    {
+        name: 'Game Stork',
+        github: 'https://github.com/ewalker518/game-go',
+        deploy: 'https://ewalker518.github.io/game-stork/',
+        photo: GG
+    },
+    {
+        name: 'DnD Character Manager',
+        github: 'https://github.com/dylanshoemaker/dnd-manager',
+        deploy: 'https://dnd-character-sheet-manager.herokuapp.com/',
+        photo: dnd
+    },
+    {
+        name: 'SalonMe',
+        github: 'https://github.com/dylanshoemaker/SalonMe',
+        deploy: 'https://salonme.herokuapp.com/',
+        photo: salonme
+    },
+    {
+        name: 'Budget Tracker PWA',
+        github: 'https://github.com/jessangarcia/budget-tracker',
+        deploy: 'https://morning-retreat-27504.herokuapp.com/',
+        photo: budget
+    },
+    {
+        name: 'Weather Dashboard',
+        github: 'https://github.com/jessangarcia/weatherdashboard',
+        deploy: 'https://jessangarcia.github.io/weatherdashboard/',
+        photo: weather
+    },
+    {
+        name: 'Run Buddy',
+        github: 'https://github.com/jessangarcia/run-buddy',
+        deploy: 'https://jessangarcia.github.io/run-buddy/',
+        photo: runbud
+    }
+    
+];
+
+export default data;
